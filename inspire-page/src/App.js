99d3470f45dc4899749bbe11931ca6d1dc39c6dd
Chevy <75184977+chavionjackson@users.noreply.github.com>
2022-06-24 17:51:18 -0700

    able to display time

--- inspire-page/src/App.js
+++ inspire-page/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Clock from "react-live-clock";
 
 function App() {
+  let time = new Date().toLocaleString();
+
+  // const ShowTime = () => {
+  //   let today = new Date();
+  //   let hour = today.getHours();
+  //   let min = today.getMinutes();
+  //   let sec = today.getSeconds();
+  //   // Set AM or PM
+  //   const amPm = hour >= 12 ? "PM" : "AM";
+
+  //   // 12hr Format
+  //   hour = hour % 12 || 12;
+  //    console.log(`${hour}:${min}:${sec} ${amPm}`);
+  //   setTimeout(ShowTime, 1000);
+  // };
+
+  // // Add Zeros
+  // function addZero(n) {
+  //   return (parseInt(n, 10) < 10 ? "0" : "") + n;
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Clock format={"HH:mm:ss"} ticking={true} timezone={"US/Pacific"} /> */}
+      <time>{time}</time>
+      <h1>
+        <span id="greeting"></span>
+        <span id="name"></span>
+      </h1>
+      <h2>What Is Your Focus For Today?</h2>
+      {/* <h2 id="focus">{}</h2> */}
     </div>
   );
 }
